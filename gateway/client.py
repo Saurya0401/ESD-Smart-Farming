b6a5963104a72aaffa2f6e34187eef25a547d26c
Saurya0401 <sp04012000@gmail.com>
2023-05-20 22:19:01 +0800

    Renamed class Gateway -> Client

--- gateway/client.py
+++ gateway/client.py
@@ -8,7 +8,7 @@ from typing import Final
 from receiver import Receiver
 
 
-class Gateway(mqtt.Client):
+class Client(mqtt.Client):
     THINGSBOARD_HOST: str = 'demo.thingsboard.io'
     THINGSBOARD_PORT: int = 1883
     ACCESS_TOKEN: str = 'td4W5nqvPdYzIYpiYOrH'
@@ -22,21 +22,21 @@ class Gateway(mqtt.Client):
     def __init__(self) -> None:
         super().__init__()
         self.sensor_data: dict[str, float] = {
-            Gateway.KEY_TEMP: 0.0,
-            Gateway.KEY_LIGHT: 0,
-            Gateway.KEY_WATER: 0,
+            Client.KEY_TEMP: 0.0,
+            Client.KEY_LIGHT: 0,
+            Client.KEY_WATER: 0,
         }
-        self.username_pw_set(Gateway.ACCESS_TOKEN)
-        self.connect(Gateway.THINGSBOARD_HOST, Gateway.THINGSBOARD_PORT, 60)
+        self.username_pw_set(Client.ACCESS_TOKEN)
+        self.connect(Client.THINGSBOARD_HOST, Client.THINGSBOARD_PORT, 60)
         self.receiver = Receiver()
 
     def get_data(self) -> None:
         if not self.receiver.receive():
             raise ValueError
         # TODO: parse received data
-        self.sensor_data[Gateway.KEY_TEMP] = round(uniform(20.0, 30.0), 4)
-        self.sensor_data[Gateway.KEY_LIGHT] = randrange(1000, 4001)
-        self.sensor_data[Gateway.KEY_WATER] = randrange(0, 11)
+        self.sensor_data[Client.KEY_TEMP] = round(uniform(20.0, 30.0), 4)
+        self.sensor_data[Client.KEY_LIGHT] = randrange(1000, 4001)
+        self.sensor_data[Client.KEY_WATER] = randrange(0, 11)
 
     def publish_sensor_data(self) -> None:
         self.loop_start()
@@ -50,12 +50,12 @@ class Gateway(mqtt.Client):
                 self.publish(
                     'v1/devices/me/telemetry', 
                     json.dumps(self.sensor_data), 
-                    Gateway.DEFAULT_QOS
+                    Client.DEFAULT_QOS
                 )
                 print('Published:')
                 for key, val in self.sensor_data.items():
                     print(f'\t->{key} = {val}')
-                sleep(Gateway.DELAY_SECONDS)
+                sleep(Client.DELAY_SECONDS)
         except KeyboardInterrupt:
             print('Sensor data publish stopped')
             self.receiver.radio.powerDown()
@@ -63,7 +63,7 @@ class Gateway(mqtt.Client):
         
 
 def main():
-    client: Gateway = Gateway()
+    client: Client = Client()
     client.publish_sensor_data()
 
 
