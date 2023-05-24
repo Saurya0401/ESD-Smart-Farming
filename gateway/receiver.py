dd3709905cf4e2dad14347255a9070259dd50135
Saurya0401 <sp04012000@gmail.com>
2023-05-24 13:15:13 +0800

    Updated receiver code

--- gateway/receiver.py
+++ gateway/receiver.py
@@ -1,6 +1,6 @@
 from RF24 import RF24, RF24_PA_LOW
 
-from time import monotonic
+from time import monotonic, sleep
 from typing import Optional, Final
 
 
@@ -8,7 +8,7 @@ class Receiver:
     CSN_PIN: Final[int] = 0  # connected to GPIO8
     CE_PIN: Final[int] = 22  # connected to GPIO22
     P_SIZE: Final[int] = 32  # default payload size
-    READ_ADDR: Final[bytes] = b'1Node'
+    READ_ADDR: Final[bytes] = 0xF1F1F0F0E0
 
     def __init__(self) -> None:
         self.radio = RF24(Receiver.CE_PIN, Receiver.CSN_PIN)
@@ -18,20 +18,22 @@ class Receiver:
         self.radio.openReadingPipe(1, Receiver.READ_ADDR)
         self.radio.payloadSize = Receiver.P_SIZE
         self.radio.printPrettyDetails()
-        # self.radio.powerDown()
+        self.radio.startListening()
 
-    def receive(self, timeout: int = 6) -> Optional[str]:
+    def receive(self, timeout: int = 5) -> Optional[bytes]:
         self.radio.startListening()
         start_timer: float = monotonic()
-        recv_data: Optional[str] = None
-        while (monotonic() - start_timer) > timeout:
-            if not self.radio.available():
-                continue
-            recv_data = self.radio.read(self.radio.payloadSize)
-            print(f'received: {recv_data}')
+        while (monotonic() - start_timer) < timeout:
+            if self.radio.available():
+                recv_data: bytes = self.radio.read(self.radio.payloadSize)
+                print(f'received: {recv_data}, len = {len(recv_data)}')
+                return recv_data
         print('Receiver timed out with no data received')
+        return None
+    
+    def stop(self) -> None:
         self.radio.stopListening()
-        return recv_data
+        self.radio.powerDown()
     
 
 if __name__ == '__main__':
